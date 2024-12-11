449540743031a1bc038eb030c9886c1ada8d6544
gsarvagya45 <gsarvagya45@gmail.com>
2024-12-12 00:58:35 +0530

    added new commands

---
+++ index.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    printf("this is a new repo");
+    return 0;
+}
+
+//command to access folder /make new repo from visualstudio only
+/*
+
+cd 'folder ka name'
+{isse hum us folder me access kar payenge files ko}
+
+cd ..
+{through this we can exit a certain directory}
+
+mkdir 'folder/repo name'
+{through this we can make new directory}
+*/
+
+
